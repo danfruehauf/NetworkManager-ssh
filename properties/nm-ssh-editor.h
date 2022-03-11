e3289f0dd800244f24e91eaa3546def78bc658cb
Lubomir Rintel <lkundrak@v3.sk>
2022-03-11 13:16:27 +0100

    properties: port to Gtk4
    Now that we don't use any deprecated widgets/properteis, it's fairly
    straightforward.

--- properties/nm-ssh-editor.h
+++ properties/nm-ssh-editor.h
@@ -24,7 +24,24 @@
 #ifndef _NM_SSH_EDITOR_H_
 #define _NM_SSH_EDITOR_H_
 
-#include <glib-object.h>
+#include <gtk/gtk.h>
+
+#if !GTK_CHECK_VERSION(4,0,0)
+#define gtk_editable_set_text(editable,text)		gtk_entry_set_text(GTK_ENTRY(editable), (text))
+#define gtk_editable_get_text(editable)			gtk_entry_get_text(GTK_ENTRY(editable))
+#define gtk_widget_get_root(widget)			gtk_widget_get_toplevel(widget)
+#define gtk_check_button_get_active(button)		gtk_toggle_button_get_active(GTK_TOGGLE_BUTTON(button))
+#define gtk_check_button_set_active(button, active)	gtk_toggle_button_set_active(GTK_TOGGLE_BUTTON(button), active)
+#define gtk_window_destroy(window)			gtk_widget_destroy(GTK_WIDGET (window))
+#define gtk_window_set_hide_on_close(window, hide)						\
+	G_STMT_START {										\
+		G_STATIC_ASSERT(hide);								\
+		g_signal_connect_swapped (G_OBJECT (window), "delete-event",			\
+					  G_CALLBACK (gtk_widget_hide_on_delete), window);	\
+	} G_STMT_END
+
+typedef void GtkRoot;
+#endif
 
 #define SSH_TYPE_EDITOR            (ssh_editor_get_type ())
 #define SSH_EDITOR(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), SSH_TYPE_EDITOR, SshEditor))
